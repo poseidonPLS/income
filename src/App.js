eca24d0550a613fbdab66636f3fe9fed77d3967f
donseelim <donseelim@gmail.com>
2024-05-19 22:56:31 +0800

    New

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App() {
     <div className="App">
       <h1>PulseChain Validator Balance and Income</h1>
       <p className="disclaimer">
-        The accuracy of this information relies on the indexing of the beacon explorer. No responsibility is taken for any inaccuracies. Please note that positive values indicate income, while a single negative value, shown in red, represent a withdrawal to the wallet.
+        The accuracy of this information relies on the indexing of the beacon explorer. No responsibility is taken for any inaccuracies. Please note that positive values indicate income, while a single negative value (shown in red) represent a withdrawal to the wallet.
       </p>
       <div className="input-container">
         <input
